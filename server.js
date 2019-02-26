ca33988cafd8dc5adec0f9121c6b76946ef021d5
Gaby Casillas <csgaby7@gmail.com>
2019-02-25 22:25:43 -0600

    configuration to port 8000

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log("hola");
-process.exit();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('APP listening on port '+process.env.PORT);
+})
