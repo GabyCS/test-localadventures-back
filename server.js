f59bfd5ad6aac6cccd9c05e1a97a6689ee264202
Gabriela Casillas Salas <csgaby7@gmail.com>
2019-02-28 13:15:25 -0600

    Se incluyo el paquete de cors

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 const routes = require('./api/routes');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors());
 
 app.use('/', routes);
 
